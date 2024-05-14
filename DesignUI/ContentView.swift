dcb3e6387e1a9e902839abdb3623efb3b082762f
Aniket Patil <aniket.p@reapmind.com>
2024-05-14 11:30:13 +0530

    Initial Commit

---
+++ DesignUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignUI
+//
+//  Created by Aniket Patil on 14/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DesignUI/DesignUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignUIApp.swift
+//  DesignUI
+//
+//  Created by Aniket Patil on 14/05/24.
+//
+
+import SwiftUI
+
+@main
+struct DesignUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
